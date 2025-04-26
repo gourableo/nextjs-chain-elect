2982e778a11e64a7163e1d8c6cd18b41024ee495
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-04-26 17:09:03 +0530

    update abi and addresses
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,11 @@
 import { ContractInfo } from "@/types";
 import VoterDatabase from "./VoterDatabase.json" assert { type: "json" };
+import CandidateDatabase from "./CandidateDatabase.json" assert { type: "json" };
 
 const typedVoterDb = VoterDatabase as ContractInfo;
+const typedCandidateDb = CandidateDatabase as ContractInfo;
 
 export const VOTER_DB_ADDRESS = typedVoterDb.addresses.sepolia;
 export const VOTER_DB_ABI = typedVoterDb.abi;
+export const CANDIDATE_DB_ADDRESS = typedCandidateDb.addresses.sepolia;
+export const CANDIDATE_DB_ABI = typedCandidateDb.abi;
