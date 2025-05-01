20928c39ce7f2c856b5089bdc8fcee4e38c7971f
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-05-01 14:53:37 +0530

    ThemeDropdownContent: disable the buttons if their corresponding theme is set
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/components/header/theme/ThemeDropdownContent.tsx
+++ src/components/header/theme/ThemeDropdownContent.tsx
@@ -3,16 +3,16 @@ import { MoonIcon, SunIcon, LaptopIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export function ThemeDropdownContent() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   return (
     <>
-      <DropdownMenuItem onClick={() => setTheme("light")}>
+      <DropdownMenuItem onClick={() => setTheme("light")} disabled={theme === "light"}>
         <SunIcon className="mr-2 h-4 w-4" /> Light
       </DropdownMenuItem>
-      <DropdownMenuItem onClick={() => setTheme("dark")}>
+      <DropdownMenuItem onClick={() => setTheme("dark")} disabled={theme === "dark"}>
         <MoonIcon className="mr-2 h-4 w-4" /> Dark
       </DropdownMenuItem>
-      <DropdownMenuItem onClick={() => setTheme("system")}>
+      <DropdownMenuItem onClick={() => setTheme("system")} disabled={theme === "system"}>
         <LaptopIcon className="mr-2 h-4 w-4" /> System
       </DropdownMenuItem>
     </>
